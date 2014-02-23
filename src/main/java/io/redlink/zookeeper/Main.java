8138cff1f44e1b7d3844865afa78a43447edcbfa
Jakob Frank <jakob.frank@redlink.co>
2014-02-23 22:00:35 +0100

    zk watches now work

--- src/main/java/io/redlink/zookeeper/Main.java
+++ src/main/java/io/redlink/zookeeper/Main.java
@@ -1,24 +1,42 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.redlink.zookeeper;
+
+import io.redlink.zookeeper.configuration.ZookeeperConfiguration;
+
+import java.io.IOException;
 
 /**
  * Created by jakob on 2/23/14.
  */
 public class Main {
+
+    public static void main(String... args) {
+        String zkConnect = "localhost:2181";
+
+        try {
+            System.out.println("Main: Start");
+            ZookeeperConfiguration zkc = new ZookeeperConfiguration(zkConnect);
+
+            System.out.println("Main: Set test=Foo");
+            zkc.setProperty("test", "foo");
+            System.out.println("Main: test=Foo");
+
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Main: test=" + zkc.getString("test"));
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    System.out.println("interrupt");
+                }
+            }
+
+            System.out.println("End");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
--- src/main/java/io/redlink/zookeeper/configuration/ZookeeperConfiguration.java
+++ src/main/java/io/redlink/zookeeper/configuration/ZookeeperConfiguration.java
@@ -1,88 +1,208 @@
 package io.redlink.zookeeper.configuration;
 
 import com.google.common.cache.*;
-import com.google.common.collect.ImmutableMap;
 import org.apache.commons.configuration.AbstractConfiguration;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.*;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class ZookeeperConfiguration extends AbstractConfiguration {
 
+    private static final Logger log = LoggerFactory.getLogger(ZookeeperConfiguration.class);
+
     private final String zkConnectionString;
     private final int zkTimeout;
-    private final LoadingCache<String, Object> cache = CacheBuilder.newBuilder()
-            .build(new CacheLoader<String, Object>() {
+    private final String zkRoot;
+
+    private final LoadingCache<String, String> cache = CacheBuilder.newBuilder()
+            .removalListener(new RemovalListener<Object, Object>() {
                 @Override
-                public Object load(String key) throws Exception {
-                    return zk.getData(key, true, null);
+                public void onRemoval(RemovalNotification<Object, Object> notification) {
+                    LoggerFactory.getLogger(this.getClass()).debug("Cache entry removed: {} ({})", notification.getKey(), notification.getCause().toString());
+                }
+            })
+            .build(new CacheLoader<String, String>() {
+                private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+                @Override
+                public String load(String key) throws Exception {
+                    log.trace("loading Key: {}", key);
+                    try {
+                        return new String(zk.getData(zkRoot + key, true, null));
+                    } catch (final Throwable t) {
+                        log.error("Cache.load: " + t.getMessage(), t);
+                        throw t;
+                    }
                 }
             });
-
+    private List<String> keyList;
     private ZooKeeper zk = null;
 
     public ZookeeperConfiguration(String zkConnectionString) throws IOException {
         this.zkConnectionString = zkConnectionString;
         this.zkTimeout = 5000;
+        this.zkRoot = "/";
+
+        zkInit();
     }
 
     private void zkInit() throws IOException {
+        final CountDownLatch sync = new CountDownLatch(1);
+        log.debug("zkInit - connecting");
         // if (zk != null) zk.close();
-        zk = new ZooKeeper(zkConnectionString, 5000, new ZKWatcher());
+        zk = new ZooKeeper(zkConnectionString, zkTimeout, new ZKWatcher(sync));
+
+        try {
+            sync.await(zkTimeout, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            throw new IOException("Could not connect", e);
+        }
+        log.info("zkInit - connected");
     }
 
     @Override
+    protected void clearPropertyDirect(String key) {
+        try {
+            zk.delete(zkRoot + key, -1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
     protected void addPropertyDirect(String key, Object value) {
-        // FIXME
-        zk.create(key, value.toString().getBytes(), null, CreateMode.PERSISTENT);
+        final String path = zkRoot + key;
+        // FIXME: this is no add but set!
+        try {
+            final Stat stat = zk.exists(path, false);
+            if (stat != null) {
+                log.debug("{} already exists, overwrite", key);
+                zk.setData(path, value.toString().getBytes(), stat.getVersion());
+            } else {
+                log.debug("new key {}", key);
+                zk.create(path, value.toString().getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return listKeys().isEmpty();
     }
 
     @Override
     public boolean containsKey(String key) {
-        return cache.get(key) != null;
+        return listKeys().contains(key);
     }
 
     @Override
     public Object getProperty(String key) {
-        return cache.get(key);
+        try {
+            return cache.get(key);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
     public Iterator<String> getKeys() {
-        return null;
+        return listKeys().iterator();
+    }
+
+    private List<String> listKeys() {
+        try {
+            // FIXME: Concurrency?
+            if (keyList == null) {
+                keyList = zk.getChildren(zkRoot, true);
+            }
+            return keyList;
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
     }
 
     private class ZKWatcher implements Watcher {
 
+        private final Logger log = LoggerFactory.getLogger(ZKWatcher.class);
+        private final CountDownLatch syncConnect;
+
+        public ZKWatcher(CountDownLatch syncConnect) {
+            this.syncConnect = syncConnect;
+        }
+
         @Override
         public void process(WatchedEvent watchedEvent) {
-            switch (watchedEvent.getState()) {
-                case Disconnected:
-                case Expired:
-                    cache.invalidateAll();
-                    zkInit();
-                    break;
-            }
-            switch (watchedEvent.getType()) {
-                case NodeDataChanged:
-                case NodeDeleted:
-                    cache.invalidate(watchedEvent.getPath());
-                    break;
-                case None:
+            if (log.isTraceEnabled()) {
+                log.trace("zk Event: {}", watchedEvent);
             }
 
+            switch (watchedEvent.getType()) {
+                case None:
+                    break;
+                case NodeDeleted:
+                case NodeDataChanged:
+                    cache.invalidate(watchedEvent.getPath().substring(zkRoot.length()));
+                    return;
+                case NodeCreated:
+                    // nop;
+                    break;
+                case NodeChildrenChanged:
+                    keyList = null;
+                    break;
+            }
+
+            switch (watchedEvent.getState()) {
+                case SyncConnected:
+                    log.trace("zk connected");
+                    syncConnect.countDown();
+                    break;
+                case Expired:
+                case Disconnected:
+                    log.info("zk {}, trying to reconnect", watchedEvent.getState());
+                    cache.invalidateAll();
+                    try {
+                        zk.close();
+                    } catch (InterruptedException e) {
+                        log.warn("exception while closing connection", e);
+                    }
+                    try {
+                        zkInit();
+                    } catch (IOException e) {
+                        throw new IllegalStateException("zk Reconnect failed", e);
+                    }
+                    break;
+                case AuthFailed:
+                    log.error("zk auth failed");
+                    throw new IllegalStateException("zk Authentication failed");
+                case ConnectedReadOnly:
+                    log.warn("zk read only");
+                    break;
+                case SaslAuthenticated:
+                    log.info("zk authenticated");
+                    break;
+            }
         }
     }
 }
